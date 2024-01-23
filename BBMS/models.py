49eae919f08de29796ddfb47c47b5575747b86fb
Subekshya Bajimayo <subekshya.211239@ncit.edu.np>
2024-01-23 22:00:06 +0545

    test

--- BBMS/models.py
+++ BBMS/models.py
@@ -3,12 +3,37 @@ from django.contrib.auth import get_user_model
 
 User=get_user_model()
 
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
 class Donor(models.Model):
- 
-    Donor_Name=models.CharField(max_length=100)
-    Donor_Age=models.IntegerField(default=10)
-    Donor_BloodType=models.CharField(max_length=50)
-    
+    Donor_Name = models.CharField(max_length=100)
+    Donor_Age = models.IntegerField(default=10)
+    Donor_BloodType = models.CharField(max_length=50)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.Donor_Name
+
+class Receipent(models.Model):
+    Receipent_Name = models.CharField(max_length=100)
+    Receipent_Age = models.IntegerField(default=10)
+    Receipent_BloodType = models.CharField(max_length=50)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.Receipent_Name
+
+class BloodBank(models.Model):
+    BloodBank_Order = models.CharField(max_length=100)
+    BloodBank_Bloodtype = models.CharField(max_length=50)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.BloodBank_Order
+
     
     
     
--- project/urls.py
+++ project/urls.py
@@ -19,6 +19,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('BBMS.urls')),
+    path('', include('BBSM.urls')),
 ]
 
