e1dd619cf8a742e9444959e6f7df841e8c31386f
Subekshya Bajimayo <subekshya.211239@ncit.edu.np>
2024-01-23 20:29:54 +0545

    giving path to app

--- BBMS/models.py
+++ BBMS/models.py
@@ -2,3 +2,13 @@ from django.db import models
 from django.contrib.auth import get_user_model
 
 User=get_user_model()
+
+class Donor(models.Model):
+ 
+    Donor_Name=models.CharField(max_length=100)
+    Donor_Age=models.IntegerField(default=10)
+    Donor_BloodType=models.CharField(max_length=50)
+    
+    
+    
+    
--- BBMS/urls.py
+++ BBMS/urls.py
@@ -1,8 +1,9 @@
 
-from django.urls import path
+from django.urls import path,include
 from .views import *
 urlpatterns = [
     path('signup/', signUp, name='signup'),
     path('login/', login, name='login'),
     path('index/',index,name='index'),
+    path('bbsm',include('bbsm.urls')),
 ]
