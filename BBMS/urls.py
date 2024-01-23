9482364b2aee569eae8133ce922cb61367104455
prabesh Raj Pant <prabeshraj.211223@ncit.edu.np>
2024-01-23 11:35:27 +0545

    login change

--- BBMS/urls.py
+++ BBMS/urls.py
@@ -2,7 +2,7 @@
 from django.urls import path
 from .views import *
 urlpatterns = [
-    path('signup/', signup, name='signup'),
-    path('login/', user_login, name='login'),
+    path('signup/', signUp, name='signup'),
+    path('login/', login, name='login'),
     path('index/',index,name='index'),
 ]
--- BBMS/views.py
+++ BBMS/views.py
@@ -1,31 +1,20 @@
 # views.py
-from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
-from .forms import SignupForm, LoginForm
-
-def signup(request):
-    if request.method == 'POST':
-        form = SignupForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('index')  # Replace 'index' with the name of your index page
-    else:
-        form = SignupForm()
-
-    return render(request, 'login.html', {'form': form})
-
-def user_login(request):
-    if request.method == 'POST':
-        form = LoginForm(request, data=request.POST)
-        if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            return redirect('index')  # Replace 'index' with the name of your index page
-    else:
-        form = LoginForm()
-
-    return render(request, 'login.html', {'form': form})
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+def signUp(request):
+    if request.method=='POST':
+        first_name=request.POST['first_name']
+        last_name=request.POST['last_name']
+        username=request.POST['username']
+        email=request.POST['email']
+        pass1=request.POST['pass1']
+        pass2=request.POST['pass2']
+        data=User.objects.create_user(first_name=first_name,last_name=last_name,username=username,email=email,password=pass1)
+        data.save()
+        return redirect('login')
+    return render(request,'login.html')
 
 def index(request):
-    return render(request,'index.html')
+    return render(request,"index.html")
+def login(request):
+    return render(request,"login.html")
