b6f6e3dd0e6132ccc48d3c169b8bc0269a1b9907
prabesh Raj Pant <prabeshraj.211223@ncit.edu.np>
2024-01-23 14:14:10 +0545

    login complete

--- BBMS/urls.py
+++ BBMS/urls.py
@@ -1,8 +1,15 @@
-
+from django.views import View
+from django.contrib import admin
 from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 from .views import *
+
 urlpatterns = [
-    path('signup/', signUp, name='signup'),
-    path('login/', login, name='login'),
-    path('index/',index,name='index'),
+    path('admin/', admin.site.urls),
+    path('',index, name='index'),
+    path('register/',register, name='register'),
+    path('register/auth/', AuthView.as_view(), name='auth'),
+    path('login/',user_login, name='login'),
 ]
