bacd3432535383348ffbae08d7cd4d80f69356b3
bsuyog1 <suyog.211241@gmail.com>
2024-01-23 20:50:00 +0545

    all templates

---
+++ index.js
@@ -0,0 +1,18 @@
+let todos=[
+    {title:"html",status:"completed"},
+    {title:"css",status:"completed"},
+    {titlr:"js", status:"running"}
+
+]
+console.log(todos[0].title+"is"todos[0].status)
+console.log(todos[1].title+"is"todos[1].status)
+console.log(todos[2].title+"is"todos[2].status)
+console.log(`${todod[0].title}${todos[0].status}`)
+let firstName="suyog"
+let lastName="bajimayo"
+let fullName=firstName+" "+lastName
+console.log("fullName")
+let fullName=`${firstName}${lastname}`
+console.log(fullName)
+
+
